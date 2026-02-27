70c198c5c9af9cd5f76c45ccde937a525480b0c8
OnlyCryptic <bossekasiri2@gmail.com>
2026-02-27 21:46:17 +0000

    Update speed.lua

--- Modules/Player/speed.lua
+++ Modules/Player/speed.lua
@@ -1,13 +1,8 @@
--- [[ Arwa Hub - ميزة السرعة المطورة ]]
+-- [[ Cryptic Hub - السرعة ]]
 return function(Tab, UI)
     local player = game.Players.LocalPlayer
-    
     Tab:AddSpeedControl("سرعة المشي", function(active, value)
         local hum = player.Character and player.Character:FindFirstChild("Humanoid")
-        if hum then
-            hum.WalkSpeed = active and value or 16
-        end
+        if hum then hum.WalkSpeed = active and value or 16 end
     end)
-    
-    Tab:AddParagraph("ملاحظة: السرعة العادية هي 16.")
 end
