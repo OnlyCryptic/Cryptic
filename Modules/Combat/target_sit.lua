c181c739f96631f2eed70d1c25b80efb64b8dbcf
OnlyCryptic <bossekasiri2@gmail.com>
2026-02-28 12:57:23 +0000

    Update target_sit.lua

--- Modules/Combat/target_sit.lua
+++ Modules/Combat/target_sit.lua
@@ -1,18 +1,23 @@
 return function(Tab, UI)
+    local runService = game:GetService("RunService")
+    local lp = game.Players.LocalPlayer
     local isSitting = false
-    game:GetService("RunService").Heartbeat:Connect(function()
-        local target = _G.ArwaTarget
-        local lp = game.Players.LocalPlayer
-        if isSitting and target and target.Character and target.Character:FindFirstChild("Head") then
-            lp.Character.HumanoidRootPart.Velocity = Vector3.new(0,0,0)
-            lp.Character.HumanoidRootPart.CFrame = target.Character.Head.CFrame * CFrame.new(0, 2.2, 0)
+
+    Tab:AddToggle("🪑 الجلوس على رأس الهدف", function(active)
+        isSitting = active
+        if lp.Character and lp.Character:FindFirstChild("Humanoid") then
+            lp.Character.Humanoid.Sit = active
         end
     end)
 
-    Tab:AddToggle("🪑 جلوس على الرأس", function(active)
-        isSitting = active
-        if game.Players.LocalPlayer.Character:FindFirstChild("Humanoid") then
-            game.Players.LocalPlayer.Character.Humanoid.Sit = active
+    runService.Heartbeat:Connect(function()
+        local target = _G.ArwaTarget
+        if isSitting and target and target.Character and target.Character:FindFirstChild("Head") then
+            local root = lp.Character and lp.Character:FindFirstChild("HumanoidRootPart")
+            if root then
+                root.Velocity = Vector3.new(0,0,0)
+                root.CFrame = target.Character.Head.CFrame * CFrame.new(0, 2.2, 0)
+            end
         end
     end)
 end
