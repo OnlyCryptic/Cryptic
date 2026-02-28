923428e21305b2b50423a780a3ecd4d47cf1f5e7
OnlyCryptic <bossekasiri2@gmail.com>
2026-02-28 12:56:34 +0000

    Update target_tp.lua

--- Modules/Combat/target_tp.lua
+++ Modules/Combat/target_tp.lua
@@ -1,11 +1,13 @@
 return function(Tab, UI)
-    Tab:AddButton("🚀 انتقال للهدف", function()
+    Tab:AddButton("🚀 انتقال إلى الهدف", function()
         local target = _G.ArwaTarget
         local lp = game.Players.LocalPlayer
-        if target and target.Character and target.Character:FindFirstChild("HumanoidRootPart") then
-            lp.Character.HumanoidRootPart.CFrame = target.Character.HumanoidRootPart.CFrame * CFrame.new(0, 3, 0)
+        if target and target.Character and target.Character:FindFirstChild("HumanoidRootPart") then 
+            if lp.Character and lp.Character:FindFirstChild("HumanoidRootPart") then
+                lp.Character.HumanoidRootPart.CFrame = target.Character.HumanoidRootPart.CFrame * CFrame.new(0, 3, 0) 
+            end
         else
-            UI:Notify("⚠️ حدد لاعباً أولاً!")
+            UI:Notify("⚠️ حدد هدفاً أولاً!")
         end
     end)
 end
