74f02e284633424e7108d70cc13757965e78aee4
OnlyCryptic <bossekasiri2@gmail.com>
2026-02-28 13:48:10 +0000

    Update main.lua

--- main.lua
+++ main.lua
@@ -17,7 +17,7 @@ local Cryptic = {
         -- التقسيم الجديد هنا:
         ["استهداف لاعب"] = { 
             Folder = "Combat", 
-            Files = {"target_select", "target_tp", "target_aimbot", "target_sit", "target_mimic"} 
+            Files = {"target_select", "target_tp", "target_aimbot", "target_sit", "target_mimic", "target_fling"} 
         },
         
         ["قسم السيرفر"] = { Folder = "Misc", Files = {"server", "rejoin"} }
