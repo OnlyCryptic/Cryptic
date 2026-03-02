d22b7209bf6f6bad0ec89cc80f5eec2912e799c9
OnlyCryptic <bossekasiri2@gmail.com>
2026-03-02 23:17:23 +0000

    Update main.lua

--- main.lua
+++ main.lua
@@ -16,7 +16,7 @@ local Cryptic = {
     Structure = {
         ["معلومات"] = { Folder = "Misc", Files = {"info"} },
         ["قسم اللاعب"] = { Folder = "Player", Files = {"speed", "fly", "noclip", "antifling", "wallwalk", "walkfling", "nofall", "infinitejump"} },
-        ["أدوات"] = { Folder = "Misc", Files = {"tptool", "esp", "emotes", "camera", "shiftlock", "anti_block"} },
+        ["أدوات"] = { Folder = "Misc", Files = {"tptool", "esp", "emotes", "camera", "shiftlock"} },
         ["استهداف لاعب"] = { Folder = "Combat", Files = {"target_select", "target_tp", "target_spectate", "target_aimbot", "target_sit", "target_mimic", "target_fling"} },
         ["قسم السيرفر"] = { Folder = "Misc", Files = {"server", "rejoin"} },
         ["خدع"] = { Folder = "Combat", Files = {"hitbox", "anime_aura", "invisibility", "zero_gravity", "fullbright", "carry", "magnet"} }
