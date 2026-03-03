f87a3cae1a35d5b952ad7d79095360fc758dc242
OnlyCryptic <bossekasiri2@gmail.com>
2026-03-03 00:08:55 +0000

    Update speed.lua

--- Modules/Player/speed.lua
+++ Modules/Player/speed.lua
@@ -1,8 +1,17 @@
--- [[ Cryptic Hub - السرعة ]]
+-- [[ Cryptic Hub - السرعة المطورة ]]
+-- المطور: Cryptic | التحديث: ضبط السرعة الافتراضية على 50 لتتوافق مع المحرك V4.6
+
 return function(Tab, UI)
     local player = game.Players.LocalPlayer
-    Tab:AddSpeedControl("سرعة المشي", function(active, value)
-        local hum = player.Character and player.Character:FindFirstChild("Humanoid")
-        if hum then hum.WalkSpeed = active and value or 16 end
-    end)
+    
+    -- أضفنا الرقم 50 في نهاية الدالة ليكون القيمة الافتراضية في الخانة
+    Tab:AddSpeedControl("سرعة المشي / WalkSpeed", function(active, value)
+        local char = player.Character
+        local hum = char and char:FindFirstChild("Humanoid")
+        
+        if hum then
+            -- إذا تم تفعيل الزر، يأخذ القيمة المكتوبة، وإذا أطفي يرجع للسرعة الطبيعية (16)
+            hum.WalkSpeed = active and value or 16 
+        end
+    end, 50)
 end
