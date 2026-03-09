186201cdfb480d6e5cb4fd6a5914260419fc381d
OnlyCryptic <bossekasiri2@gmail.com>
2026-03-09 17:23:09 +0000

    Update tp_method.lua

--- Modules/Experiments/tp_method.lua
+++ Modules/Experiments/tp_method.lua
@@ -1,13 +1,13 @@
 -- [[ Cryptic Hub - اختيار طريقة الانتقال ]]
 return function(Tab, UI)
-    -- القيمة الافتراضية إذا ما اختار اللاعب شيء
-    _G.CrypticTPMethod = "انتقال فوري (Instant)"
+    -- القيمة الافتراضية
+    _G.CrypticTPMethod = "انتقال فوري | Instant"
     
-    Tab:AddDropdown("طريقة الانتقال", {"انتقال فوري (Instant)", "طيران سريع (Fly/Tween)"}, function(selected)
+    Tab:AddDropdown("طريقة الانتقال | TP Method", {"انتقال فوري | Instant", "طيران سريع | Fly (Tween)"}, function(selected)
         _G.CrypticTPMethod = selected
         game:GetService("StarterGui"):SetCore("SendNotification", {
             Title = "Cryptic Hub",
-            Text = "تم التغيير إلى: " .. selected,
+            Text = "تم التغيير إلى | Changed to:\n" .. selected,
             Duration = 3
         })
     end)
