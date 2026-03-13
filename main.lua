4ed4e923654ba6482e97e91e63802e669f762691
OnlyCryptic <bossekasiri2@gmail.com>
2026-03-13 19:22:49 +0000

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,8 +1,5 @@
--- [[ Cryptic Hub - Loader ]]
--- هذا الملف في الفرع الرئيسي يقوم بتشغيل فرع التجارب (test) تلقائياً
-
 local success, err = pcall(function()
-    -- أضفنا ?v=tick لضمان تحميل آخر تحديث من GitHub بدون تأخير الكاش
+    
     loadstring(game:HttpGet("https://raw.githubusercontent.com/OnlyCryptic/Cryptic/test/main.lua?v="..tick()))()
 end)
 
