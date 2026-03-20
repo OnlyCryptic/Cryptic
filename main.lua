855e4a8f6791a5e8b859822baddd5d394b4b20ad
OnlyCryptic <bossekasiri2@gmail.com>
2026-03-20 13:41:48 +0000

    Update main.lua

--- main.lua
+++ main.lua
@@ -25,7 +25,7 @@ local Cryptic = {
 }
 
 if lp.UserId == 3875086037 then
-    Cryptic.Structure["تجارب"] = { Folder = "Experiments", Files = {"hm", "auto_apple"} }
+    Cryptic.Structure["تجارب"] = { Folder = "Experiments", Files = {"hm", "auto_apple", "AutoClicker"} }
     table.insert(Cryptic.TabsOrder, "تجارب")
 end
 
