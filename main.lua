0cf68164e7bcb34b98526d0959a0277611e0e0f0
OnlyCryptic <bossekasiri2@gmail.com>
2026-03-23 14:28:05 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -18,7 +18,7 @@ local Cryptic = {
         ["استهداف لاعب / players"] = { Folder = "Combat", Files = {"target_select", "target_tp", "target_spectate", "target_aimbot", "target_sit", "target_mimic", "target_fling", "bring_parts", "carry", "jark", "backpack", "Target_follow"} },  
         ["قسم السيرفر / server"] = { Folder = "Server", Files = {"server", "rejoin", "join_id"} },  
         ["الانتقال / Teleport"] = { Folder = "Teleport", Files = {"tp_locations"} },
-        ["اخرى / Other"] = { Folder = "Other", Files = {"animations", "vfly", "zero_gravity", "anti_block", "fling_all"} },
+        ["اخرى / Other"] = { Folder = "Other", Files = {"vfly", "animations", "zero_gravity", "anti_block", "fling_all"} },
         ["اقتراحات / Suggestions"] = { Folder = "", Files = {"suggestion"} }
     },  
     TabsOrder = {"معلومات / info", "قسم اللاعب / player", "أدوات / tools", "استهداف لاعب / players", "قسم السيرفر / server", "الانتقال / Teleport", "اخرى / Other", "اقتراحات / Suggestions"}
