40c1262270c3bd82900764651a4512fd61127062
OnlyCryptic <bossekasiri2@gmail.com>
2026-03-28 13:17:20 +0100

    Delete 120920474420629.lua

--- Maps/120920474420629.lua
+++
@@ -1,9 +0,0 @@
--- [[ Cryptic Hub - Map Script: Time Bomb Duels ]]
--- PlaceId: 120920474420629
-
-return {
-    Name = "Time Bomb Duels",
-    PlaceId = 120920474420629,
-    Description = "سكربت خاص بهذا الماب يضيف مميزات حصرية داخل لعبة Time Bomb Duels",
-    Script = [[loadstring(game:HttpGet('https://raw.githubusercontent.com/thunderXhub/ThunderXHUB/refs/heads/main/loader'))()]]
-}
