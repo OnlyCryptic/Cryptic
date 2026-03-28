f0ff2b9f7d497405ae0987342f01fc3f1ba3fa23
OnlyCryptic <bossekasiri2@gmail.com>
2026-03-28 13:32:35 +0100

    Create 2753915549.lua

---
+++ Maps/2753915549.lua
@@ -0,0 +1,21 @@
+-- [[ Cryptic Hub - Map: Blox Fruits ]]
+-- PlaceId: 2753915549
+
+return {
+    Name = "Blox Fruits",
+    PlaceId = 2753915549,
+    Scripts = {
+        {
+            Name = "Banana Hub",
+            Script = [[loadstring(game:HttpGet("https://raw.githubusercontent.com/aloaloalo322/sssdas/refs/heads/main/cc"))()]]
+        },
+        {
+            Name = "Cokka Hub",
+            Script = [[loadstring(game:HttpGet("https://raw.githubusercontent.com/UserDevEthical/Loadstring/main/CokkaHub.lua"))()]]
+        },
+        {
+            Name = "Redz Hub",
+            Script = [[loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-Old-RedzHub-with-key-system-by-NicoAntunz-82799"))()]]
+        }
+    }
+}
