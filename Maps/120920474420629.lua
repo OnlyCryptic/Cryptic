e1b26ee6620dbb0704b360393ef6513fcafde2b2
OnlyCryptic <bossekasiri2@gmail.com>
2026-03-28 13:14:33 +0100

    Update 120920474420629.lua

--- Maps/120920474420629.lua
+++ Maps/120920474420629.lua
@@ -5,5 +5,5 @@ return {
     Name = "Time Bomb Duels",
     PlaceId = 120920474420629,
     Description = "سكربت خاص بهذا الماب يضيف مميزات حصرية داخل لعبة Time Bomb Duels",
-    Script = [[loadstring(game:HttpGet('https://raw.githubusercontent.com/Arhangels-team/SpectreX-Hub/main/Time%20Bomb%20duels.lua'))()]]
+    Script = [[loadstring(game:HttpGet('https://raw.githubusercontent.com/thunderXhub/ThunderXHUB/refs/heads/main/loader'))()]]
 }
