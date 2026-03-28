9fa9be98f66c2c9af478ff475df8165697392e5a
OnlyCryptic <bossekasiri2@gmail.com>
2026-03-29 00:22:05 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -28,7 +28,7 @@ local Cryptic = {
         ["قسم اللاعب / player"] = { Folder = "Player", Files = {"speed", "fly", "noclip", "walkfling", "antifling", "wallwalk", "nofall", "infinitejump"} },  
         ["أدوات / tools"] = { Folder = "Misc", Files = {"tptool", "auto_tool", "esp", "shiftlock", "emotes", "x-ray", "fullbright", "camera"} },  
         ["استهداف لاعب / players"] = { Folder = "Combat", Files = {"target_select", "target_tp", "target_spectate", "target_aimbot", "target_sit", "target_mimic", "target_fling", "bring_parts", "carry", "jark", "backpack", "Target_follow"} },  
-        ["قسم السيرفر / server"] = { Folder = "Server", Files = {"server", "rejoin", "join_id"} },  
+        ["قسم السيرفر / server"] = { Folder = "Server", Files = {"server", "rejoin", "join_id", "players"} },  
         ["الانتقال / Teleport"] = { Folder = "Teleport", Files = {"tp_locations"} },
         ["اخرى / Other"] = { Folder = "Other", Files = {"vfly", "animations", "zero_gravity", "anti_block", "fling_all"} },
         ["اقتراحات / Suggestions"] = { Folder = "", Files = {"suggestion"} }
