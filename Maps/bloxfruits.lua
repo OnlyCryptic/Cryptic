a90233b13ebb357f0aa082cd9ae3627861ca03dd
OnlyCryptic <bossekasiri2@gmail.com>
2026-03-28 13:45:20 +0100

    Create bloxfruits.lua

---
+++ Maps/bloxfruits.lua
@@ -0,0 +1,20 @@
+-- [[ Cryptic Hub - Map: Blox Fruits ]]
+-- جميع إيديات عوالم Blox Fruits تشير لهذا الملف
+
+return {
+    Name = "Blox Fruits",
+    Scripts = {
+        {
+            Name = "Banana Hub",
+            Script = [[loadstring(game:HttpGet("https://raw.githubusercontent.com/aloaloalo322/sssdas/refs/heads/main/cc"))()]]
+        },
+        {
+            Name = "Cokka Hub",
+            Script = [[loadstring(game:HttpGet("https://raw.githubusercontent.com/UserDevEthical/Loadstring/main/CokkaHub.lua"))()]]
+        },
+        {
+            Name = "Redz Hub",
+            Script = [[loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-Old-RedzHub-with-key-system-by-NicoAntunz-82799"))()]]
+        }
+    }
+}
