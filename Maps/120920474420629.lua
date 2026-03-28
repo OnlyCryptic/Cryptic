656ee95fee93f76183d06b52b6e1ac157cf86788
OnlyCryptic <bossekasiri2@gmail.com>
2026-03-28 13:00:08 +0100

    Create Maps/120920474420629.lua

---
+++ Maps/120920474420629.lua
@@ -0,0 +1,9 @@
+-- [[ Cryptic Hub - Map Script: Time Bomb Duels ]]
+-- PlaceId: 120920474420629
+
+return {
+    Name = "Time Bomb Duels",
+    PlaceId = 120920474420629,
+    Description = "سكربت خاص بهذا الماب يضيف مميزات حصرية داخل لعبة Time Bomb Duels",
+    Script = [[loadstring(game:HttpGet('https://raw.githubusercontent.com/Arhangels-team/SpectreX-Hub/main/Time%20Bomb%20duels.lua'))()]]
+}
