cad59bdda07f1f052224ebd96a89e53edb5b4ef6
OnlyCryptic <bossekasiri2@gmail.com>
2026-03-29 12:48:54 +0100

    Update Line.lua

--- UI/Elements/Line.lua
+++ UI/Elements/Line.lua
@@ -4,7 +4,13 @@ return function(TabOps)
     local L = Instance.new("Frame", TabOps.Page)
     L.LayoutOrder = TabOps.Order
     L.Size = UDim2.new(0.95, 0, 0, 1)
-    L.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
-    L.BackgroundTransparency = 0.5
+    L.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+    L.BackgroundTransparency = 0.7
     L.BorderSizePixel = 0
+    local G = Instance.new("UIGradient", L)
+    G.Color = ColorSequence.new{
+        ColorSequenceKeypoint.new(0, Color3.fromRGB(0, 255, 150)),
+        ColorSequenceKeypoint.new(1, Color3.fromRGB(0, 150, 255))
+    }
+    G.Rotation = 0
 end
