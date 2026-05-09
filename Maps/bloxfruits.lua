1548eb1cf45cafcaffa2ffd011cbae876df0cb6d
OnlyCryptic <bossekasiri2@gmail.com>
2026-05-09 01:49:50 +0100

    snapshot of test

---
+++ Maps/bloxfruits.lua
@@ -0,0 +1,28 @@
+-- [[ Cryptic Hub - Map: Blox Fruits ]]
+-- جميع إيديات عوالم Blox Fruits تشير لهذا الملف
+
+return {
+    Name = "Blox Fruits",
+    Scripts = {
+        {
+            Name = "Banana Hub",
+            Script = [[loadstring(game:HttpGet("https://raw.githubusercontent.com/aloaloalo322/sssdas/refs/heads/main/cc"))()]]
+        },
+        {
+            Name = "QuantumOnyx (no key ✅)",
+            Script = [[loadstring(game:HttpGet("https://raw.githubusercontent.com/flazhy/QuantumOnyx/refs/heads/main/QuantumOnyx.lua"))()]]
+        },
+        {
+            Name = "MeoXHub (no key)",
+            Script = [[loadstring(game:HttpGet("https://raw.githubusercontent.com/VanHoangIOS/MeoXHub/refs/heads/main/Main.lua"))()]]
+        },
+        {
+            Name = "Cokka Hub",
+            Script = [[loadstring(game:HttpGet("https://raw.githubusercontent.com/UserDevEthical/Loadstring/main/CokkaHub.lua"))()]]
+        },
+        {
+            Name = "Redz Hub",
+            Script = [[loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-Old-RedzHub-with-key-system-by-NicoAntunz-82799"))()]]
+        }
+    }
+}
---
+++ Maps/mm2.lua
@@ -0,0 +1,11 @@
+-- [[ Cryptic Hub - Map: Murder Mystery 2 ]]
+
+return {
+    Name = "Murder Mystery 2",
+    Scripts = {
+        {
+            Name = "MM2 Script (no key)",
+            Script = [[loadstring(game:HttpGet("https://raw.githubusercontent.com/rageba1t/mm2/refs/heads/main/script.lua", true))()]]
+        }
+    }
+}
