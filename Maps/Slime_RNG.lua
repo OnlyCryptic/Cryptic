37271ab1e4714ea16a81570d0fdf15e3e388a386
OnlyCryptic <bossekasiri2@gmail.com>
2026-05-10 13:51:31 +0100

    Update Slime_RNG.lua

--- Maps/Slime_RNG.lua
+++ Maps/Slime_RNG.lua
@@ -5,6 +5,10 @@ return {
     Name = "Slime RNG",
     Scripts = {
         {
+            Name = "vaenz (no key 🔥)",
+            Script = [[loadstring(game:HttpGet("https://raw.githubusercontent.com/vaenzz/Loader/refs/heads/main/Games"))()]]
+        },
+        {
             Name = "Slime RNG Script",
             Script = [[loadstring(game:HttpGet("https://raw.githubusercontent.com/gumanba/Scripts/main/SlimeRNG"))()]]
         }
