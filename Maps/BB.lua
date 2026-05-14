f6efc98d5213df46eaab86d0c95ba334dbf8baa1
OnlyCryptic <bossekasiri2@gmail.com>
2026-05-14 17:39:13 +0100

    Create BB.lua

---
+++ Maps/BB.lua
@@ -0,0 +1,12 @@
+-- [[ Cryptic Hub - Map: Blade Ball ]]
+-- جميع إيديات Blade Ball تشير لهذا الملف
+
+return {
+    Name = "Blade Ball",
+    Scripts = {
+        {
+            Name = "Auto Parry (keyless)",
+            Script = [[loadstring(game:HttpGet("https://rawscripts.net/raw/Blade-Ball-Auto-Parry-Keyless-128675"))()]]
+        }
+    }
+}
