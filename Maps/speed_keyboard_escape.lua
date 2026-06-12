77985fac765ed797c7d0f59163e9ba94fee2b779
OnlyCryptic <bossekasiri2@gmail.com>
2026-06-12 15:13:48 +0100

    Create speed_keyboard_escape.lua

---
+++ Maps/speed_keyboard_escape.lua
@@ -0,0 +1,9 @@
+return {
+    Name = "Speed Keyboard Escape",
+    Scripts = {
+        {
+            Name = "Hoshi no key",
+            Script = [[loadstring(game:HttpGet("https://raw.githubusercontent.com/Fluxyyy333/HoshiOnTop/main/loader.lua"))()]]
+        }
+    }
+}
