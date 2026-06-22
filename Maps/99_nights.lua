f82f9af61dc6c8f79aa3630649d3693fcae643f1
OnlyCryptic <bossekasiri2@gmail.com>
2026-06-22 14:13:48 +0100

    Create Maps/99_nights.lua

---
+++ Maps/99_nights.lua
@@ -0,0 +1,12 @@
+-- [[ Cryptic Hub - Map: 99 Nights ]]
+-- جميع إيديات 99 Nights تشير لهذا الملف
+
+return {
+    Name = "99 Nights",
+    Scripts = {
+        {
+            Name = "Foxname Hub (keyless)",
+            Script = [[loadstring(game:HttpGet("https://rawscripts.net/raw/99-Nights-in-the-Forest-Foxname-Hub-Made-By-EchoBuddyYTT-110418"))()]]
+        }
+    }
+}
