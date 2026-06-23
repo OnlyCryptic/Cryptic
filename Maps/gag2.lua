74c81a67ecd1b2b757ec2262a69203d5b70005e0
OnlyCryptic <bossekasiri2@gmail.com>
2026-06-23 02:17:47 +0100

    Update gag2.lua

--- Maps/gag2.lua
+++ Maps/gag2.lua
@@ -5,19 +5,23 @@ return {
     Name = "grow a garden 2",
     Scripts = {
         {
-            Name = "Teddy hub need key",
-            Script = [[repeat task.wait() until game:IsLoaded() and game:GetService("Players") and game.Players.LocalPlayer and game.Players.LocalPlayer:FindFirstChild("PlayerGui")
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Teddyseetink/Haidepzai/refs/heads/main/main.lua"))()
-]]
+            Name = "Real Kid Hub (keyless ✅)",
+            Script = [[loadstring(game:HttpGet("https://raw.githubusercontent.com/realkidhub/realkid/refs/heads/main/main.lua"))()]]
         },
         {
-            Name = "Real Kid Hub",
-            Script = [[loadstring(game:HttpGet("https://raw.githubusercontent.com/realkidhub/realkid/refs/heads/main/main.lua"))()]]
+            Name = "Solix Hub (keyless ✅)",
+            Script = [[loadstring(game:HttpGet("https://raw.githubusercontent.com/meobeo8/a/a/a"))()]]
         },
         {
             Name = "Speed Hub (need key 🔒😔)",
             Script = [[loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()]]
+        },
+        {
+            Name = "Teddy Hub (need key 🔒😔)",
+            Script = [[repeat task.wait() until game:IsLoaded() and game:GetService("Players") and game.Players.LocalPlayer and game.Players.LocalPlayer:FindFirstChild("PlayerGui")
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Teddyseetink/Haidepzai/refs/heads/main/main.lua"))()
+]]
         }
     }
 }
