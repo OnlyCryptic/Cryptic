2f4a402abb6483fae5c1381bbe6d0b6b93e5c7cc
OnlyCryptic <bossekasiri2@gmail.com>
2026-07-04 17:24:40 +0100

    Create sell_lemons.lua

---
+++ Maps/sell_lemons.lua
@@ -0,0 +1,12 @@
+-- [[ Cryptic Hub - Map: Sell Lemons ]]
+-- جميع إيديات Sell Lemons تشير لهذا الملف
+
+return {
+    Name = "Sell Lemons",
+    Scripts = {
+        {
+            Name = "HacxHub (no key 🔥)",
+            Script = [[loadstring(game:HttpGet("https://raw.githubusercontent.com/Zettabyte-ShinX/HacxHub/refs/heads/main/SellLemons"))()]]
+        }
+    }
+}
