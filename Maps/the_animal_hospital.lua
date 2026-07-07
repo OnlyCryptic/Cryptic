fa1ce4ae2cf2935eebf856d27b9b80cb757098ff
OnlyCryptic <bossekasiri2@gmail.com>
2026-07-07 19:48:35 +0100

    Update the_animal_hospital.lua

--- Maps/the_animal_hospital.lua
+++ Maps/the_animal_hospital.lua
@@ -5,8 +5,12 @@ return {
     Name = "The Animal Hospital",
     Scripts = {
         {
-            Name = "need 60s to loading",
+            Name = "Mini Hub (brief lag on startup)",
             Script = [[loadstring(game:HttpGet("https://raw.githubusercontent.com/MinityD/roblox-utils/main/animal%20hospital.lua"))()]]
+        },
+        {
+            Name = "Cyraa Hub",
+            Script = [[loadstring(game:HttpGet("https://raw.githubusercontent.com/LynX99-9/komtolmmek2script/refs/heads/main/CyraaHub.lua", true))()]]
         }
     }
 }
