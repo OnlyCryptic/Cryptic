c7b0b34a782389b29aa1c536b12caf30c3da204d
OnlyCryptic <bossekasiri2@gmail.com>
2026-07-07 19:29:20 +0100

    Create the_animal_hospital.lua

---
+++ Maps/the_animal_hospital.lua
@@ -0,0 +1,12 @@
+-- [[ Cryptic Hub - Map: The Animal Hospital ]]
+-- جميع إيديات The Animal Hospital تشير لهذا الملف
+
+return {
+    Name = "The Animal Hospital",
+    Scripts = {
+        {
+            Name = "need 60s to loading",
+            Script = [[loadstring(game:HttpGet("https://raw.githubusercontent.com/MinityD/roblox-utils/main/animal%20hospital.lua"))()]]
+        }
+    }
+}
