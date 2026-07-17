96ca5365ca16418fcd90c2ac84a55fb2c94c5069
OnlyCryptic <bossekasiri2@gmail.com>
2026-07-17 04:32:59 +0100

    Create prison_life.lua

---
+++ Maps/prison_life.lua
@@ -0,0 +1,12 @@
+-- [[ Cryptic Hub - Map: Prison Life ]]
+-- جميع إيديات Prison Life تشير لهذا الملف
+
+return {
+    Name = "Prison Life",
+    Scripts = {
+        {
+            Name = "Prison Life Script",
+            Script = [[loadstring(game:HttpGet("https://raw.githubusercontent.com/zenss555a/script/refs/heads/main/Prison-Life.lua", true))()]]
+        }
+    }
+}
