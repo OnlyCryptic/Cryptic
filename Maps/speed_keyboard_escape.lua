3349e1c00d210f4f2dd394f30a873f956bd6dfab
OnlyCryptic <bossekasiri2@gmail.com>
2026-07-19 04:31:03 +0100

    Update speed_keyboard_escape.lua

--- Maps/speed_keyboard_escape.lua
+++ Maps/speed_keyboard_escape.lua
@@ -8,6 +8,10 @@ return {
         {
             Name = "Hoshi Hub",
             Script = [[loadstring(game:HttpGet("https://raw.githubusercontent.com/Fluxyyy333/HoshiOnTop/main/loader.lua"))()]]
+        },
+        {
+            Name = "Kato Hub",
+            Script = [[loadstring(game:HttpGet("https://raw.githubusercontent.com/Lilwero20/KatoHub/refs/heads/main/KatoHub"))()]]
         }
     }
 }
