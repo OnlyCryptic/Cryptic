95e3629b0f4671711d32532b34797205c9317c0d
OnlyCryptic <bossekasiri2@gmail.com>
2026-08-11 15:03:03 +0100

    Update speed_keyboard_escape.lua

--- Maps/speed_keyboard_escape.lua
+++ Maps/speed_keyboard_escape.lua
@@ -1,25 +1,25 @@
 return {
-Name = "Speed Keyboard Escape",
-Scripts = {
-{
-Name = "No Key ✅",
-Script = [[loadstring(game:HttpGet("https://raw.githubusercontent.com/Terfiscript1/KitagawaHub-1speed/refs/heads/main/KitagawaHub%2B1speed"))()]
-},
-{
-Name = "Luxy Hub (⚠️)",
-Script = [[loadstring(game:HttpGet("https://www.luxyhub.space/api/loader/luxyhub"))()]
-},
-{
-Name = "Hoshi Hub 🔒",
-Script = [[loadstring(game:HttpGet("https://raw.githubusercontent.com/Fluxyyy333/HoshiOnTop/main/loader.lua"))()]
-},
-{
-Name = "Kato Hub 🔒",
-Script = [[loadstring(game:HttpGet("https://raw.githubusercontent.com/Lilwero20/KatoHub/refs/heads/main/KatoHub"))()]
-},
-{
-Name = "Ajjans Hub 🔒",
-Script = [[loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/359e97f8618e9008afe5f496184ebb7c.lua"))()]
-}
-}
+    Name = "Speed Keyboard Escape",
+    Scripts = {
+        {
+            Name = "No Key ✅",
+            Script = [[loadstring(game:HttpGet("https://raw.githubusercontent.com/Terfiscript1/KitagawaHub-1speed/refs/heads/main/KitagawaHub%2B1speed"))()]]
+        },
+        {
+            Name = "Luxy Hub (error⚠️)",
+            Script = [[loadstring(game:HttpGet("https://www.luxyhub.space/api/loader/luxyhub"))()]]
+        },
+        {
+            Name = "Hoshi Hub (🔒)",
+            Script = [[loadstring(game:HttpGet("https://raw.githubusercontent.com/Fluxyyy333/HoshiOnTop/main/loader.lua"))()]]
+        },
+        {
+            Name = "Kato Hub (🔒)",
+            Script = [[loadstring(game:HttpGet("https://raw.githubusercontent.com/Lilwero20/KatoHub/refs/heads/main/KatoHub"))()]]
+        },
+        {
+            Name = "Ajjans Hub (🔒)",
+            Script = [[loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/359e97f8618e9008afe5f496184ebb7c.lua"))()]]
+        }
+    }
 }
