f598aaf3e471a7875de1447a110a87a30e732fdf
OnlyCryptic <bossekasiri2@gmail.com>
2026-08-26 08:00:48 +0100

    Create Maps/steal_an_egg.lua

---
+++ Maps/steal_an_egg.lua
@@ -0,0 +1,12 @@
+-- [[ Cryptic Hub - Map: Steal An Egg ]]
+-- جميع إيديات Steal An Egg تشير لهذا الملف
+
+return {
+    Name = "Steal An Egg",
+    Scripts = {
+        {
+            Name = "Ouroboros (keyless)",
+            Script = [[loadstring(game:HttpGet("https://raw.githubusercontent.com/joustingmatch/Ouroboros/main/loader.lua"))()]]
+        }
+    }
+}
